0446df9167915ed90591a442d7f33c56bb01dc6c
shawngmc <shawngmc@gmail.com>
2020-11-11 19:04:18 -0500

    display updates

--- temp-and-light.py
+++ temp-and-light.py
@@ -199,84 +199,6 @@ def get_cpu_temperature():
         temp = int(temp) / 1000.0
     return temp
 
-
-def correct_humidity(humidity, temperature, corr_temperature):
-    dewpoint = temperature - ((100 - humidity) / 5)
-    corr_humidity = 100 - (5 * (corr_temperature - dewpoint))
-    return min(100, corr_humidity)
-
-
-def analyse_pressure(pressure, t):
-    global time_vals, pressure_vals, trend
-    if len(pressure_vals) > num_vals:
-        pressure_vals = pressure_vals[1:] + [pressure]
-        time_vals = time_vals[1:] + [t]
-
-        # Calculate line of best fit
-        line = numpy.polyfit(time_vals, pressure_vals, 1, full=True)
-
-        # Calculate slope, variance, and confidence
-        slope = line[0][0]
-        intercept = line[0][1]
-        variance = numpy.var(pressure_vals)
-        residuals = numpy.var([(slope * x + intercept - y) for x, y in zip(time_vals, pressure_vals)])
-        r_squared = 1 - residuals / variance
-
-        # Calculate change in pressure per hour
-        change_per_hour = slope * 60 * 60
-        # variance_per_hour = variance * 60 * 60
-
-        mean_pressure = numpy.mean(pressure_vals)
-
-        # Calculate trend
-        if r_squared > 0.5:
-            if change_per_hour > 0.5:
-                trend = ">"
-            elif change_per_hour < -0.5:
-                trend = "<"
-            elif -0.5 <= change_per_hour <= 0.5:
-                trend = "-"
-
-            if trend != "-":
-                if abs(change_per_hour) > 3:
-                    trend *= 2
-    else:
-        pressure_vals.append(pressure)
-        time_vals.append(t)
-        mean_pressure = numpy.mean(pressure_vals)
-        change_per_hour = 0
-        trend = "-"
-
-    # time.sleep(interval)
-    return (mean_pressure, change_per_hour, trend)
-
-
-def describe_pressure(pressure):
-    """Convert pressure into barometer-type description."""
-    if pressure < 970:
-        description = "storm"
-    elif 970 <= pressure < 990:
-        description = "rain"
-    elif 990 <= pressure < 1010:
-        description = "change"
-    elif 1010 <= pressure < 1030:
-        description = "fair"
-    elif pressure >= 1030:
-        description = "dry"
-    else:
-        description = ""
-    return description
-
-
-def describe_humidity(humidity):
-    """Convert relative humidity into good/bad description."""
-    if 40 < humidity < 60:
-        description = "good"
-    else:
-        description = "bad"
-    return description
-
-
 def describe_light(light):
     """Convert light level in lux to descriptive value."""
     if light < 50:
@@ -321,6 +243,7 @@ sun_radius = 50
 # Fonts
 font_sm = ImageFont.truetype(UserFont, 12)
 font_lg = ImageFont.truetype(UserFont, 14)
+font_xl = ImageFont.truetype(UserFont, 20)
 
 # Margins
 margin = 3
@@ -337,16 +260,6 @@ factor = 2.25
 cpu_temps = [get_cpu_temperature()] * 5
 
 disp_imperial = True
-
-# Set up light sensor
-ltr559 = LTR559()
-
-# Pressure variables
-pressure_vals = []
-time_vals = []
-num_vals = 1000
-interval = 1
-trend = "-"
 
 # Keep track of time elapsed
 start_time = time.time()
@@ -399,17 +312,6 @@ while True:
     temp_icon = Image.open(f"{path}/icons/temperature.png")
     img.paste(temp_icon, (margin, 18), mask=temp_icon)
 
-    # Humidity
-    humidity = bme280.get_humidity()
-    corr_humidity = correct_humidity(humidity, temperature, corr_temperature)
-    humidity_string = f"{corr_humidity:.0f}%"
-    img = overlay_text(img, (68, 48), humidity_string, font_lg, align_right=True)
-    spacing = font_lg.getsize(humidity_string)[1] + 1
-    humidity_desc = describe_humidity(corr_humidity).upper()
-    img = overlay_text(img, (68, 48 + spacing), humidity_desc, font_sm, align_right=True, rectangle=True)
-    humidity_icon = Image.open(f"{path}/icons/humidity-{humidity_desc.lower()}.png")
-    img.paste(humidity_icon, (margin, 48), mask=humidity_icon)
-
     # Light
     light = ltr559.get_lux()
     light_string = f"{int(light):,}"
@@ -419,18 +321,6 @@ while True:
     img = overlay_text(img, (WIDTH - margin - 1, 18 + spacing), light_desc, font_sm, align_right=True, rectangle=True)
     light_icon = Image.open(f"{path}/icons/bulb-{light_desc.lower()}.png")
     img.paste(humidity_icon, (80, 18), mask=light_icon)
-
-    # Pressure
-    pressure = bme280.get_pressure()
-    t = time.time()
-    mean_pressure, change_per_hour, trend = analyse_pressure(pressure, t)
-    pressure_string = f"{int(mean_pressure):,} {trend}"
-    img = overlay_text(img, (WIDTH - margin, 48), pressure_string, font_lg, align_right=True)
-    pressure_desc = describe_pressure(mean_pressure).upper()
-    spacing = font_lg.getsize(pressure_string.replace(",", ""))[1] + 1
-    img = overlay_text(img, (WIDTH - margin - 1, 48 + spacing), pressure_desc, font_sm, align_right=True, rectangle=True)
-    pressure_icon = Image.open(f"{path}/icons/weather-{pressure_desc.lower()}.png")
-    img.paste(pressure_icon, (80, 48), mask=pressure_icon)
 
     # Display image
     disp.display(img)
